d6ee69c002b8defce4bc55946a35b588e159420e
MarinEASV <150655341+MarinEASV@users.noreply.github.com>
2024-10-15 10:45:42 +0200

    language switcher change

--- footer.php
+++ footer.php
@@ -114,39 +114,15 @@
 
         <!--- language selector --->
         <div class="dropdown dropup">
-          <a class="dropdown-toggle text-black" href="#" id="Dropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false"> <i class="flag-united-kingdom flag m-0 me-1"></i>English </a>
+    <a class="dropdown-toggle text-black" href="#" id="Dropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+        <i class="flag-united-kingdom flag m-0 me-1"></i>English
+    </a>
 
-          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="Dropdown">
-            <li>
-              <a class="dropdown-item text-black" href="#"><i class="flag-united-kingdom flag"></i>English <i class="fa fa-check text-success ms-2"></i></a>
-            </li>
-            <li><hr class="dropdown-divider" /></li>
-            <li>
-              <a class="dropdown-item text-black" href="#"><i class="flag-poland flag"></i>Polski</a>
-            </li>
-            <li>
-              <a class="dropdown-item text-black" href="#"><i class="flag-china flag"></i>中文</a>
-            </li>
-            <li>
-              <a class="dropdown-item text-black" href="#"><i class="flag-japan flag"></i>日本語</a>
-            </li>
-            <li>
-              <a class="dropdown-item text-black" href="#"><i class="flag-germany flag"></i>Deutsch</a>
-            </li>
-            <li>
-              <a class="dropdown-item text-black" href="#"><i class="flag-france flag"></i>Français</a>
-            </li>
-            <li>
-              <a class="dropdown-item text-black" href="#"><i class="flag-spain flag"></i>Español</a>
-            </li>
-            <li>
-              <a class="dropdown-item text-black" href="#"><i class="flag-russia flag"></i>Русский</a>
-            </li>
-            <li>
-              <a class="dropdown-item text-black" href="#"><i class="flag-portugal flag"></i>Português</a>
-            </li>
-          </ul>
-        </div>
+    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="Dropdown">
+        <?php pll_the_languages( array( 'dropdown' => 0 ) ); ?>
+    </ul>
+</div>
+
         <!--- language selector --->
       </div>
     </div>
